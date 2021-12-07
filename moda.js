7faae010281f80c8a4f0e9a18b511929cc472545
Pedro Gutierrez <pedropg.rojas@gmail.com>
2021-12-07 12:37:31 -0300

    finalizando el taller 3

--- moda.js
+++ moda.js
@@ -1,2 +1,22 @@
-const lista = [1, 3, 5, 3, 5, 2, 3, 4, 6,];
+function calcularModa(lista) {
+    const listaCount = {};
+    lista.map(
+        function (elemento) {
+            if (listaCount[elemento] == true) {
+                listaCount[elemento] += 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
+        }
+    );
+    const listaArray = Object.entries(listaCount).sort(
+        function (prev, next) {
+         return prev[1] - next[1];
+        }
+    );
+    const moda = listaArray[listaArray.length - 1];
+
+    return moda;
+}
+
 
